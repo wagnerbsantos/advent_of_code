5a178fb08cd43b9074bfa7f418af93a9ee9a2f7c
Wagner Braga dos Santos <wag.097@gmail.com>
2025-12-02 10:30:48 -0300

    day4

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,18 @@ mod common;
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() {
     let before = Instant::now();
-    let result = day3::day3::part_1();
+    let result = day4::day4::part_1();
     let elapsed = before.elapsed();
     println!("Part 1: {}", result);
     println!("Elapsed time: {:.2?}", elapsed);
 
-    let before = Instant::now();
-    let result = day3::day3::part_2();
-    let elapsed = before.elapsed();
-    println!("Part 2: {}", result);
-    println!("Elapsed time: {:.2?}", elapsed);
+    // let before = Instant::now();
+    // let result = day3::day3::part_2();
+    // let elapsed = before.elapsed();
+    // println!("Part 2: {}", result);
+    // println!("Elapsed time: {:.2?}", elapsed);
 }
